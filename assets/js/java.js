f5ee0e482cd8320ad2350cd675b2885c5519c6e2
Ahmed Bakr <117715752+Ahmedbakr74@users.noreply.github.com>
2023-10-01 13:16:56 +0300

    Add files via upload

---
+++ assets/js/java.js
@@ -0,0 +1,15 @@
+let el = document.querySelector('.up');
+
+window.onscroll= function(){
+    this.scrollY >= 1200 ?  el.classList.add('show') : el.classList.remove('show');
+}; 
+
+el.onclick = function(){
+    window.scrollTo({
+        top: 0,
+        behavior:'smooth',
+        });
+} ;  
+        
+
+
